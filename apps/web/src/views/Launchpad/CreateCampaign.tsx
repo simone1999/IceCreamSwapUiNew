c8338d010e99506cc0bb8c5cc76bec9a402b50bf
vincent <vmcodes05@gmail.com>
2023-09-22 23:34:33 -0400

    form menu

--- apps/web/src/views/Launchpad/CreateCampaign.tsx
+++ apps/web/src/views/Launchpad/CreateCampaign.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Input, Spinner, Text, useModal } from '@pancakeswap/uikit'
+import { Button, ButtonMenu, ButtonMenuItem, Flex, Input, Row, Spinner, Text, useModal } from '@pancakeswap/uikit'
 import AppWrapper from 'components/AppWrapper'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { FormProvider, useForm } from 'react-hook-form'
@@ -29,6 +29,12 @@ export const CreateCampaign: React.FC = () => {
   const { address } = useAccount()
   const [paid, setPaid] = useState<string | null>(null)
   const [loading, setLoading] = useState<boolean>(true)
+  const [index, setIndex] = useState(0)
+  const handleClick = (newIndex) => {
+    setIndex(newIndex)
+
+    window.scrollTo(0, 0)
+  }
 
   const getPaid = async () => {
     const response = await fetch(`/api/kyc-info/${address}`)
@@ -44,7 +50,7 @@ export const CreateCampaign: React.FC = () => {
   }
 
   useEffect(() => {
-    if (!paid) {
+    if (!paid && address) {
       getPaid()
     }
     // eslint-disable-next-line
@@ -53,140 +59,162 @@ export const CreateCampaign: React.FC = () => {
   return (
     <AppWrapper title={t('Create Campaign')} subtitle={t('Create your own campaign in seconds')}>
       {loading && (
-        <Flex justifyContent="center" marginY="14px">
+        <Flex justifyContent="center" marginY="28px">
           <Spinner />
         </Flex>
       )}
 
-      {paid && (
-        <FormProvider {...form}>
-          <form
-            onSubmit={handleSubmit((data) => {
-              setFormValues(data)
-              onPresentCreateModal()
-            })}
-            style={{ display: 'flex', flexDirection: 'column', gap: '1em' }}
-          >
-            {paid === 'NOT MINTED' && (
+      <FormProvider {...form}>
+        <form
+          onSubmit={handleSubmit((data) => {
+            setFormValues(data)
+            onPresentCreateModal()
+          })}
+          style={{ display: 'flex', flexDirection: 'column', gap: '1em' }}
+        >
+          {paid === 'NOT MINTED' && (
+            <>
               <span style={{ margin: 'auto' }}>
                 <FormError>You must first become KYC verified.</FormError>
               </span>
-            )}
 
-            {paid === 'MINTED' && (
-              <>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Token Address')}</Text>
-                  <Input placeholder={t('Token Address')} {...register('tokenAddress')} />
-                  {errors.tokenAddress && <FormError>{errors.tokenAddress.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Description')}</Text>
-                  <Input placeholder="Description" {...register('description')} />
-                  {errors.description && <FormError>{errors.description.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Soft Cap')}</Text>
-                  <Input type="number" placeholder={t('Soft Cap')} {...register('softCap')} />
-                  {errors.softCap && <FormError>{errors.softCap.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Hard Cap')}</Text>
-                  <Input type="number" placeholder={t('Hard Cap')} {...register('hardCap')} />
-                  {errors.hardCap && <FormError>{errors.hardCap.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Minimum Allowed')}</Text>
-                  <Input type="number" placeholder={t('Minimum Allowed')} {...register('minAllowed')} />
-                  {errors.minAllowed && <FormError>{errors.minAllowed.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Maximum Allowed')}</Text>
-                  <Input type="number" placeholder={t('Maximum Allowed')} {...register('maxAllowed')} />
-                  {errors.maxAllowed && <FormError>{errors.maxAllowed.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Rate')}</Text>
-                  <Input type="number" placeholder={t('Rate')} {...register('rate')} />
-                  {errors.rate && <FormError>{errors.rate.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Pool Rate')}</Text>
-                  <Input type="number" placeholder={t('Pool Rate')} {...register('poolRate')} />
-                  {errors.poolRate && <FormError>{errors.poolRate.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Liquidity Rate')}</Text>
-                  <Input type="number" placeholder={t('Liquidity Rate')} {...register('liquidityRate')} />
-                  {errors.liquidityRate && <FormError>{errors.liquidityRate.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Start Date')}</Text>
-                  <Input type="date" placeholder={t('Start Date')} {...register('startDate')} />
-                  {errors.startDate && <FormError>{errors.startDate.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('End Date')}</Text>
-                  <Input type="date" placeholder={t('End Date')} {...register('endDate')} />
-                  {errors.endDate && <FormError>{errors.endDate.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Website')}</Text>
-                  <Input placeholder="Website" {...register('website')} />
-                  {errors.website && <FormError>{errors.website.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Twitter')}</Text>
-                  <Input placeholder="Twitter" {...register('twitter')} />
-                  {errors.twitter && <FormError>{errors.twitter.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Telegram')}</Text>
-                  <Input placeholder="Telegram" {...register('telegram')} />
-                  {errors.telegram && <FormError>{errors.telegram.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Discord')}</Text>
-                  <Input placeholder="Discord" {...register('discord')} />
-                  {errors.discord && <FormError>{errors.discord.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Reddit')}</Text>
-                  <Input placeholder="Reddit" {...register('reddit')} />
-                  {errors.reddit && <FormError>{errors.reddit.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('GitHub')}</Text>
-                  <Input placeholder="GitHub" {...register('github')} />
-                  {errors.github && <FormError>{errors.github.message}</FormError>}
-                </Flex>
-                <Flex flexDirection="column">
-                  <Text marginBottom="7px">{t('Banner')}</Text>
-                  <FileInput
-                    accept={{
-                      'image/png': ['.png'],
-                      'image/jpeg': ['.jpeg', '.jpg'],
-                    }}
-                    name="banner"
-                  />
-                  {errors.banner && <FormError>{errors.banner.message}</FormError>}
-                </Flex>
-                <Button type="submit" variant="primary">
-                  {t('Create Campaign')}
-                </Button>
-              </>
-            )}
-
-            {paid === 'NOT MINTED' && (
               <Link href="/kyc">
-                <Button as="a" height="40px" width="100%">
+                <Button as="a" height="40px" width="100%" marginBottom="14px">
                   {t('Proceed to KYC')}
                 </Button>
               </Link>
-            )}
-          </form>
-        </FormProvider>
-      )}
+            </>
+          )}
+
+          {paid === 'MINTED' && (
+            <>
+              {index === 0 && (
+                <>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Token Address')}</Text>
+                    <Input placeholder={t('Token Address')} {...register('tokenAddress')} />
+                    {errors.tokenAddress && <FormError>{errors.tokenAddress.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Description')}</Text>
+                    <Input placeholder="Description" {...register('description')} />
+                    {errors.description && <FormError>{errors.description.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Soft Cap')}</Text>
+                    <Input type="number" placeholder={t('Soft Cap')} {...register('softCap')} />
+                    {errors.softCap && <FormError>{errors.softCap.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Hard Cap')}</Text>
+                    <Input type="number" placeholder={t('Hard Cap')} {...register('hardCap')} />
+                    {errors.hardCap && <FormError>{errors.hardCap.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Minimum Allowed')}</Text>
+                    <Input type="number" placeholder={t('Minimum Allowed')} {...register('minAllowed')} />
+                    {errors.minAllowed && <FormError>{errors.minAllowed.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Maximum Allowed')}</Text>
+                    <Input type="number" placeholder={t('Maximum Allowed')} {...register('maxAllowed')} />
+                    {errors.maxAllowed && <FormError>{errors.maxAllowed.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Rate')}</Text>
+                    <Input type="number" placeholder={t('Rate')} {...register('rate')} />
+                    {errors.rate && <FormError>{errors.rate.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Pool Rate')}</Text>
+                    <Input type="number" placeholder={t('Pool Rate')} {...register('poolRate')} />
+                    {errors.poolRate && <FormError>{errors.poolRate.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Liquidity Rate')}</Text>
+                    <Input type="number" placeholder={t('Liquidity Rate')} {...register('liquidityRate')} />
+                    {errors.liquidityRate && <FormError>{errors.liquidityRate.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Start Date')}</Text>
+                    <Input type="date" placeholder={t('Start Date')} {...register('startDate')} />
+                    {errors.startDate && <FormError>{errors.startDate.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('End Date')}</Text>
+                    <Input type="date" placeholder={t('End Date')} {...register('endDate')} />
+                    {errors.endDate && <FormError>{errors.endDate.message}</FormError>}
+                  </Flex>
+                </>
+              )}
+
+              {index === 1 && (
+                <>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Website')}</Text>
+                    <Input placeholder="Website" {...register('website')} />
+                    {errors.website && <FormError>{errors.website.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Twitter')}</Text>
+                    <Input placeholder="Twitter" {...register('twitter')} />
+                    {errors.twitter && <FormError>{errors.twitter.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Telegram')}</Text>
+                    <Input placeholder="Telegram" {...register('telegram')} />
+                    {errors.telegram && <FormError>{errors.telegram.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Discord')}</Text>
+                    <Input placeholder="Discord" {...register('discord')} />
+                    {errors.discord && <FormError>{errors.discord.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Reddit')}</Text>
+                    <Input placeholder="Reddit" {...register('reddit')} />
+                    {errors.reddit && <FormError>{errors.reddit.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('GitHub')}</Text>
+                    <Input placeholder="GitHub" {...register('github')} />
+                    {errors.github && <FormError>{errors.github.message}</FormError>}
+                  </Flex>
+                  <Flex flexDirection="column">
+                    <Text marginBottom="7px">{t('Banner')}</Text>
+                    <FileInput
+                      accept={{
+                        'image/png': ['.png'],
+                        'image/jpeg': ['.jpeg', '.jpg'],
+                      }}
+                      name="banner"
+                    />
+                    {errors.banner && <FormError>{errors.banner.message}</FormError>}
+                  </Flex>
+                </>
+              )}
+
+              <Row>
+                <ButtonMenu
+                  activeIndex={index}
+                  onItemClick={handleClick}
+                  fullWidth
+                  scale="sm"
+                  variant="subtle"
+                  marginY="14px"
+                >
+                  <ButtonMenuItem>Campaign Info</ButtonMenuItem>
+                  <ButtonMenuItem>Social Details</ButtonMenuItem>
+                </ButtonMenu>
+              </Row>
+
+              <Button type="submit" variant="primary" marginBottom="14px">
+                {t('Create Campaign')}
+              </Button>
+            </>
+          )}
+        </form>
+      </FormProvider>
     </AppWrapper>
   )
 }
