d1a90ac31cc290cb1e9d27d45cac270c19c3a518
simon <simon@klein-homepage.de>
2024-06-25 16:36:35 +0300

    cleaned up CreateCampaign.tsx

--- apps/web/src/views/Launchpad/CreateCampaign.tsx
+++ apps/web/src/views/Launchpad/CreateCampaign.tsx
@@ -12,7 +12,6 @@ import {
   Heading,
   Progress,
   Box,
-  Alert,
 } from '@pancakeswap/uikit'
 import AppWrapper from 'components/AppWrapper'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -25,12 +24,54 @@ import { useEffect, useState } from 'react'
 import { useTranslation } from '@pancakeswap/localization'
 import { useAccount } from 'wagmi'
 import Link from 'next/link'
-import { useUser } from 'strict/hooks/useUser'
 import { useActiveChainId } from 'hooks/useActiveChainId'
 import { ICE } from '@pancakeswap/tokens'
-import { useFormContext } from 'react-hook-form'
+import ConnectWalletButton from "components/ConnectWalletButton";
 
 export const CreateCampaign: React.FC = () => {
+
+  const { chainId } = useActiveChainId()
+  const raisedToken = ICE[chainId]
+
+  const content = [
+    {
+      heading: 'Basic token details',
+      text: 'Enter the basic details about your Token launch',
+    },
+    {
+      heading: 'Presale timing',
+      text: 'Specify when your presale begins and ends',
+    },
+    {
+      heading: 'Soft and Hard Cap',
+      text: `Set the minimum and maximum amount of ${raisedToken.symbol} for this Campaign to raise`,
+    },
+    {
+      heading: 'Min and Max Contribution',
+      text: `Min/Max ${raisedToken.symbol} users are allowed to contribute to your Campaign. Set to 0 to disable`,
+    },
+    {
+      heading: 'Presale and Listing Rate',
+      text: `Amount of tokens user receive for 1 ${raisedToken.symbol} Token`,
+    },
+    {
+      heading: 'Automatic Liquidity provision',
+      text: `percentage of raised ${raisedToken.symbol} used to automatically provide Liquidity and till when this liquidity will be locked`,
+    },
+    {
+      heading: 'Vesting',
+      text: 'Example: 80% over 2 months means users receive 20% immediately and 80% distributed over 2 month. Set to 0 to disable.',
+    },
+    {
+      heading: 'Project Socials',
+      text: 'Specify the socials of your Project. Website is required, rest is optional',
+    },
+    {
+      heading: 'Preview',
+      text: 'Check the parameters for your Campaign',
+    },
+  ]
+
   const schema = useSchema()
   const { t } = useTranslation()
   const form = useForm<FormValues>({
@@ -48,35 +89,32 @@ export const CreateCampaign: React.FC = () => {
 
   const [onPresentCreateModal] = useModal(<CreateModal formValues={formValues} />, true, true, 'tokenCreateModal')
 
-  const { address } = useAccount()
+  const { address, isConnected } = useAccount()
   const [kyced, setKyced] = useState<boolean>(false)
   const [loading, setLoading] = useState<boolean>(true)
   const [index, setIndex] = useState(0)
-  const user = useUser()
-  const { chainId } = useActiveChainId()
-  const [progress, setProgress] = useState(0)
-  const [isHovered, setIsHovered] = useState(false)
-
-  const raisedToken = ICE[chainId]
+  const [progress, setProgress] = useState(100 / (content.length + 1))
 
   const goToNextPage = () => {
-    if (index < 8) {
-      // Since index is 0-based, 5 represents the 6th page
-      const nextIndex = index + 1 < content.length ? index + 1 : 0
+    // Ensure index is within bounds
+    if (index < content.length) {
+      const nextIndex = index + 1
       setIndex(nextIndex)
-      progress < 100 ? setProgress(progress + 12.5) : setProgress(0)
+      setProgress(100 * (nextIndex + 1) / (content.length + 1))
     }
   }
   const goToPreviousPage = () => {
+    // Ensure index is within bounds
     if (index > 0) {
-      // Ensure index is within bounds
-      setIndex(index - 1)
+      const nextIndex = index - 1
+      setIndex(nextIndex)
       // Decrease progress but ensure it doesn't go below 0
-      setProgress(progress > 0 ? progress - 12.5 : 0)
+      setProgress(100 * (nextIndex + 1) / (content.length + 1))
     }
   }
 
   const getKyced = async () => {
+    setLoading(true)
     const response = await fetch(`/api/kyc-info/${address}`)
     const data = await response.json()
 
@@ -90,50 +128,13 @@ export const CreateCampaign: React.FC = () => {
   }
 
   useEffect(() => {
-    if (!kyced && user.data?.isLoggedIn) {
+    if (!isConnected) {
+      setLoading(false)
+      setKyced(false)
+    } else if (!kyced) {
       getKyced()
     }
-
-    setTimeout(() => {
-      if (user.data?.isLoggedIn === false) {
-        setLoading(false)
-        setKyced(false)
-      }
-    }, 3500)
-    // eslint-disable-next-line
-  }, [user, kyced])
-
-  const content = [
-    { heading: 'Enter your Token Address', text: 'Lorem ipsum dolor sit amet.', infoText: 'Info about Token Address' },
-    { heading: 'Set your Presale Dates', text: 'Lorem ipsum dolor sit amet.', infoText: 'Info about Presale Dates' },
-    {
-      heading: 'Set your Soft and Hard Cap',
-      text: 'Lorem ipsum dolor sit amet.',
-      infoText: 'Info about Soft and Hard Cap',
-    },
-    {
-      heading: 'Set your Min and Max Contribution',
-      text: 'Lorem ipsum dolor sit amet.',
-      infoText: 'Info about Min and Max Contribution',
-    },
-    {
-      heading: 'Set your Presale and Listing Rate',
-      text: 'Lorem ipsum dolor sit amet.',
-      infoText: 'Info about Presale and Listing Rate',
-    },
-    {
-      heading: 'Set your Liquidity Rate and Unlock Date',
-      text: 'Lorem ipsum dolor sit amet.',
-      infoText: 'Info about Liquidity Rate and Unlock Date',
-    },
-    {
-      heading: 'Set your Vesting Percentage and End Date',
-      text: 'Lorem ipsum dolor sit amet.',
-      infoText: 'Info about Vesting Percentage and End Date',
-    },
-    { heading: 'Add your Socials', text: 'Lorem ipsum dolor sit amet.', infoText: 'Info about Socials' },
-    { heading: 'Preview', text: 'Lorem ipsum dolor sit amet.', infoText: 'Preview' },
-  ]
+  }, [isConnected, address])
 
   const currentContent = content[index]
 
@@ -154,17 +155,25 @@ export const CreateCampaign: React.FC = () => {
           style={{ display: 'flex', flexDirection: 'column', gap: '1em' }}
         >
           {!kyced && !loading && (
-            <>
-              <span style={{ margin: 'auto', textAlign: 'center', width: '75%' }}>
-                <FormError>You must first become KYC verified or login to your KYC verified account.</FormError>
-              </span>
+            !isConnected?
+              <>
+                <Heading as="h1" fontSize="20px" fontWeight="bold" marginBottom="7px">
+                  Please connect your Wallet
+                </Heading>
+                <ConnectWalletButton/>
+              </>
+            :
+              <>
+                <span style={{ margin: 'auto', textAlign: 'center', width: '75%' }}>
+                  <FormError>You must first become KYC verified to create a campaign.</FormError>
+                </span>
 
-              <Link href="/kyc">
-                <Button as="a" height="40px" width="100%" marginBottom="7px">
-                  {t('Proceed to KYC')}
-                </Button>
-              </Link>
-            </>
+                <Link href="/kyc">
+                  <Button as="a" height="40px" width="100%" marginBottom="7px">
+                    {t('Proceed to KYC')}
+                  </Button>
+                </Link>
+              </>
           )}
 
           {kyced && (
@@ -173,65 +182,21 @@ export const CreateCampaign: React.FC = () => {
                 <div
                   style={{
                     padding: '16px',
-                    width: '400px',
+                    width: '100%',
                   }}
                 >
-                  <Box>
-                    <Progress variant="round" primaryStep={progress} />
-                  </Box>
+                <Box>
+                  <Progress variant="round" primaryStep={progress} />
+                </Box>
                 </div>
               </Flex>
-
-              <Flex flexDirection="row" justifyContent="center" style={{ marginLeft: 20 }}>
-                <Flex flexDirection="column" justifyItems="center" alignItems="center">
-                  <Heading as="h1" fontSize="20px" fontWeight="bold" marginBottom="7px">
-                    {currentContent.heading}
-                  </Heading>
-                  <Text fontSize="14px" marginBottom="7px">
-                    {currentContent.text}
-                  </Text>
-                </Flex>
-                <div style={{ position: 'relative', display: 'inline-block' }}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 50 50"
-                    width="20px"
-                    height="20px"
-                    style={{ marginLeft: 20 }}
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
-                  >
-                    <path
-                      fill="white"
-                      d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 25 11 A 3 3 0 0 0 22 14 A 3 3 0 0 0 25 17 A 3 3 0 0 0 28 14 A 3 3 0 0 0 25 11 z M 21 21 L 21 23 L 22 23 L 23 23 L 23 36 L 22 36 L 21 36 L 21 38 L 22 38 L 23 38 L 27 38 L 28 38 L 29 38 L 29 36 L 28 36 L 27 36 L 27 21 L 26 21 L 22 21 L 21 21 z"
-                    />
-                  </svg>
-                  {isHovered && (
-                    <div
-                      style={{
-                        position: 'absolute',
-                        top: '25px', // Adjust based on SVG height and desired spacing
-                        left: '50%',
-                        transform: 'translateX(-50%)',
-                        padding: '5px',
-                        backgroundColor: 'white',
-                        border: '1px solid black',
-                        borderRadius: '5px',
-                        zIndex: 1000,
-                        width: '100px', // Adjust width as needed
-                        minHeight: '10px', // Use minHeight for flexible height based on content
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                      }}
-                    >
-                      <Text fontSize="10px" marginBottom="7px" color="black" textAlign="center">
-                        {currentContent.infoText}
-                      </Text>
-                    </div>
-                  )}
-                </div>
+              <Flex flexDirection="column" justifyItems="center" alignItems="center">
+                <Heading as="h1" fontSize="20px" fontWeight="bold" marginBottom="7px">
+                  {currentContent.heading}
+                </Heading>
+                <Text fontSize="14px" marginBottom="7px" textAlign={"center"} maxWidth={"450px"}>
+                  {currentContent.text}
+                </Text>
               </Flex>
               {index === 0 && (
                 <>
@@ -290,7 +255,7 @@ export const CreateCampaign: React.FC = () => {
                 <>
                   <Flex flexDirection="column">
                     <Text marginBottom="7px">
-                      {t('Minimum %raisedTokenSymbol% a contributor is allowed to contribute', {
+                      {t('Min contribution', {
                         raisedTokenSymbol: raisedToken.symbol,
                       })}
                     </Text>
@@ -299,7 +264,7 @@ export const CreateCampaign: React.FC = () => {
                   </Flex>
                   <Flex flexDirection="column">
                     <Text marginBottom="7px">
-                      {t('Maximum %raisedTokenSymbol% a contributor is allowed to contribute', {
+                      {t('Max contribution', {
                         raisedTokenSymbol: raisedToken.symbol,
                       })}
                     </Text>
@@ -311,13 +276,13 @@ export const CreateCampaign: React.FC = () => {
               {index === 4 && (
                 <>
                   <Flex flexDirection="column">
-                    <Text marginBottom="7px">{t(`Tokens per 1 ${raisedToken.symbol} at Presale`)}</Text>
+                    <Text marginBottom="7px">{t(`Presale Rate`)}</Text>
                     <Input placeholder={t('Presale Rate')} {...register('rate')} />
                     {errors.rate && <FormError>{errors.rate.message}</FormError>}
                   </Flex>
                   <Flex flexDirection="column">
                     <Text marginBottom="7px">
-                      {t(`Tokens per 1 ${raisedToken.symbol} after Presale (DEX listing)`)}
+                      {t(`Rate after presale (lower)`)}
                     </Text>
                     <Input placeholder={t('Listing Rate')} {...register('poolRate')} />
                     {errors.poolRate && <FormError>{errors.poolRate.message}</FormError>}
@@ -328,9 +293,7 @@ export const CreateCampaign: React.FC = () => {
                 <>
                   <Flex flexDirection="column">
                     <Text marginBottom="7px">
-                      {t('percentage of raised %raisedTokenSymbol% used to provide Liquidity', {
-                        raisedTokenSymbol: raisedToken.symbol,
-                      })}
+                      Liquidity percentage
                     </Text>
                     <Input placeholder={t('Liquidity %')} {...register('liquidityRate')} />
                     {errors.liquidityRate && <FormError>{errors.liquidityRate.message}</FormError>}
@@ -409,22 +372,22 @@ export const CreateCampaign: React.FC = () => {
               {index === 8 && (
                 <Flex flexDirection="column" justifyContent="center">
                   {Object.entries(watchedFormValues).map(([key, value], index) => {
-                    const stringValue = JSON.stringify(value, null, 2)
+                    const stringValue = String(value)
                     const labels = [
                       'Token Address',
-                      'Description',
+                      'Project Description',
                       'Presale start',
                       'Presale end',
                       'Soft Cap',
                       'Hard Cap',
                       'Minimum contribute',
                       'Maximum contribute',
-                      'Tokens per 1$ at Presale',
-                      'Tokens per 1$ after Presale (DEX listing)',
-                      'Percentage to provide Liquidity',
+                      'Presale rate',
+                      'Listing rate',
+                      'Liquidity %',
                       'Liquidity unlock date',
-                      'Contributor vesting percentage',
-                      'End of vesting period',
+                      'Vesting percentage',
+                      'Vesting end',
                       'Website',
                       'Twitter',
                       'Telegram',
@@ -435,24 +398,29 @@ export const CreateCampaign: React.FC = () => {
                     return (
                       <Flex
                         key={key}
-                        flexDirection="row"
-                        justifyContent="center"
-                        alignItems="center"
-                        marginBottom="7px"
+                        flexDirection="column"
                       >
-                        <div style={{ width: '100%' }}>{labels[index]}:</div>
-                        <Text
-                          key={index} // Assuming each stringValue is unique for simplicity
-                          style={{
-                            whiteSpace: 'nowrap', // 'normal' allows text to wrap
-                            overflow: 'hidden', // Hide overflow
-                            textOverflow: 'ellipsis', // Add ellipsis to indicate hidden text
-                            fontSize: stringValue.length > 50 ? '10px' : 'inherit',
-                            width: '75%', // Adjust width as needed
-                          }}
+                        <Flex
+                          flexDirection="row"
+                          justifyContent="center"
+                          alignItems="center"
+                          marginBottom="7px"
                         >
-                          {value ? stringValue : '-'}
-                        </Text>
+                          <div style={{ width: '100%' }}>{labels[index]}:</div>
+                          <Text
+                            key={index} // Assuming each stringValue is unique for simplicity
+                            style={{
+                              whiteSpace: 'nowrap', // 'normal' allows text to wrap
+                              overflow: 'hidden', // Hide overflow
+                              textOverflow: 'ellipsis', // Add ellipsis to indicate hidden text
+                              fontSize: stringValue.length > 50 ? '10px' : 'inherit',
+                              width: '75%', // Adjust width as needed
+                            }}
+                          >
+                            {value ? stringValue : '-'}
+                          </Text>
+                        </Flex>
+                        {errors[key] && <FormError>{errors[key].message}</FormError>}
                       </Flex>
                     )
                   })}
@@ -465,9 +433,10 @@ export const CreateCampaign: React.FC = () => {
                     {[<ButtonMenuItem>Back</ButtonMenuItem>]}
                   </ButtonMenu>
 
-                  <Button type="submit" variant="primary" marginBottom="7px">
+                  <Button type="submit" variant="primary" marginBottom="7px" disabled={Object.keys(errors).length > 0}>
                     {t('Create Campaign')}
                   </Button>
+                  {Object.keys(errors).length > 0 && <FormError>Not all Values are valid, please correct them</FormError>}
                 </Column>
               )}
             </>
