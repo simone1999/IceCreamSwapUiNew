06eee46f9086b7e7499561b061296633d21aa693
simon <simon@klein-homepage.de>
2024-09-03 14:18:53 +0300

    added Core bridged BTC to priceHelperTokens so BTC based liquidity farms can be priced properly

--- packages/farms/constants/common/index.ts
+++ packages/farms/constants/common/index.ts
@@ -1,5 +1,5 @@
 import { ChainId, ERC20Token } from '@pancakeswap/sdk'
-import { bscTokens } from '@pancakeswap/tokens'
+import { coreTokens } from '@pancakeswap/tokens'
 import type { CommonPrice } from '../../src/fetchFarmsV3'
 import type { FarmV3SupportedChainId } from '../../src'
 
@@ -12,6 +12,10 @@ export const CHAIN_ID_TO_CHAIN_NAME = {
 } satisfies Record<FarmV3SupportedChainId, string>
 
 export const priceHelperTokens: Record<number, PriceHelper> = {
+  [ChainId.CORE]: {
+    chain: 'core',
+    list: [coreTokens.coreBTC],
+  },
 }
 
 // for testing purposes
