5b1032f9cda5371a7e7a11d6beba66e72d5080a4
simon <simon@klein-homepage.de>
2024-09-04 19:05:57 +0300

    tried to decrease dependency hell. Not sure whether it helped.

--- apps/web/src/components/ErrorBoundary/SentryErrorBoundary.tsx
+++ apps/web/src/components/ErrorBoundary/SentryErrorBoundary.tsx
@@ -33,7 +33,7 @@ export function SentryErrorBoundary({ children }) {
             </Flex>
             {process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' && (
               <pre>
-                <code>{error?.message}</code>
+                <code>{error instanceof Error ? error.message : String(error)}</code>
                 <code>{componentStack}</code>
               </pre>
             )}
