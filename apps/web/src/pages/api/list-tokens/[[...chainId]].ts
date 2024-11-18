824ee7f9ec8842f32434c6a9d8ab6173dad5585a
simon <simon@klein-homepage.de>
2024-11-18 14:53:52 +0200

    specifying chainId as optional path parameter rather than in body for /api/list-tokens

--- apps/web/src/pages/api/list-tokens/[[...chainId]].ts
+++ apps/web/src/pages/api/list-tokens/[[...chainId]].ts
@@ -1,11 +1,13 @@
 import { prisma } from '@icecreamswap/database'
 
 export default async function handler(req, res) {
-  const { chainId } = req.body
+  const { chainId } = req.query
   const tokens = await prisma.token.findMany({
-    where: {
-      chainId,
-    },
+    where: chainId
+      ? {
+        chainId: Number(chainId),
+      }
+      : undefined,
   })
   res.json(tokens)
 }
