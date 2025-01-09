030beca440d944e43bc1704b22bca2bafd222d71
SHAYA Mario <66465021+marioshaya@users.noreply.github.com>
2025-01-09 14:12:19 +0300

    footer

--- apps/web/src/views/Home/components/Footer.tsx
+++ apps/web/src/views/Home/components/Footer.tsx
@@ -1,8 +1,8 @@
 import { styled } from 'styled-components'
-import { Flex, Heading, Text, useMatchBreakpoints, Button, TwitterIcon, TelegramIcon, DiscordIcon } from '@pancakeswap/uikit'
+import { Flex, Heading, Text, Button, TwitterIcon, TelegramIcon } from '@pancakeswap/uikit'
 import { useTranslation } from '@pancakeswap/localization'
-import Container from 'components/Layout/Container'
-import { useWeb3React } from '@pancakeswap/wagmi'
+// import Container from 'components/Layout/Container'
+// import { useWeb3React } from '@pancakeswap/wagmi'
 import SunburstSvg from './SunburstSvg'
 
 const BgWrapper = styled.div`
@@ -33,55 +33,55 @@ const Wrapper = styled(Flex)`
   overflow: hidden;
 `
 
-const FloatingPancakesWrapper = styled(Container)`
-  overflow: hidden;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  visibility: hidden;
+// const FloatingPancakesWrapper = styled(Container)`
+//   overflow: hidden;
+//   position: absolute;
+//   width: 100%;
+//   height: 100%;
+//   top: 0;
+//   left: 50%;
+//   transform: translateX(-50%);
+//   visibility: hidden;
 
-  ${({ theme }) => theme.mediaQueries.md} {
-    visibility: visible;
-  }
-`
+//   ${({ theme }) => theme.mediaQueries.md} {
+//     visibility: visible;
+//   }
+// `
 
-const TopLeftImgWrapper = styled(Flex)`
-  position: absolute;
-  left: 0;
-  top: 0;
-`
+// const TopLeftImgWrapper = styled(Flex)`
+//   position: absolute;
+//   left: 0;
+//   top: 0;
+// `
 
-const BottomRightImgWrapper = styled(Flex)`
-  position: absolute;
-  right: 0;
-  bottom: 0;
-`
+// const BottomRightImgWrapper = styled(Flex)`
+//   position: absolute;
+//   right: 0;
+//   bottom: 0;
+// `
 
-const topLeftImage = {
-  path: '/images/home/flying-pancakes/',
-  attributes: [
-    { src: '1-bottom', alt: 'Pancake flying on the bottom' },
-    { src: '1-left', alt: 'Pancake flying on the left' },
-    { src: '1-top', alt: 'Pancake flying on the top' },
-  ],
-}
+// const topLeftImage = {
+//   path: '/images/home/flying-pancakes/',
+//   attributes: [
+//     { src: '1-bottom', alt: 'Pancake flying on the bottom' },
+//     { src: '1-left', alt: 'Pancake flying on the left' },
+//     { src: '1-top', alt: 'Pancake flying on the top' },
+//   ],
+// }
 
-const bottomRightImage = {
-  path: '/images/home/flying-pancakes/',
-  attributes: [
-    { src: '2-bottom', alt: 'Pancake flying on the bottom' },
-    { src: '2-top', alt: 'Pancake flying on the top' },
-    { src: '2-right', alt: 'Pancake flying on the right' },
-  ],
-}
+// const bottomRightImage = {
+//   path: '/images/home/flying-pancakes/',
+//   attributes: [
+//     { src: '2-bottom', alt: 'Pancake flying on the bottom' },
+//     { src: '2-top', alt: 'Pancake flying on the top' },
+//     { src: '2-right', alt: 'Pancake flying on the right' },
+//   ],
+// }
 
 const Footer = () => {
   const { t } = useTranslation()
-  const { account } = useWeb3React()
-  const { isTablet, isDesktop } = useMatchBreakpoints()
+  // const { account } = useWeb3React()
+  // const { isTablet, isDesktop } = useMatchBreakpoints()
 
   return (
     <>
@@ -112,7 +112,7 @@ const Footer = () => {
           <Button
             as="a"
             title={t('IceCreamSwap Twitter')}
-            href="/twitter"
+            href="https://x.com/mermaidswap_xyz"
             target="_blank"
             variant="primary"
             scale="md"
@@ -123,7 +123,7 @@ const Footer = () => {
           <Button
             as="a"
             title={t('IceCreamSwap Telegram')}
-            href="/telegram"
+            href="https://t.me/mermaidswapofficial"
             target="_blank"
             variant="primary"
             scale="md"
@@ -131,7 +131,7 @@ const Footer = () => {
           >
             <TelegramIcon color="currentColor" mr="8px" /> {t('Telegram')}
           </Button>
-          <Button
+          {/* <Button
             as="a"
             title={t('IceCreamSwap Discord')}
             href="/discord"
@@ -141,7 +141,7 @@ const Footer = () => {
             mr="8px"
           >
             <DiscordIcon color="currentColor" mr="8px" /> {t('Discord')}
-          </Button>
+          </Button> */}
         </Flex>
       </Wrapper>
     </>
