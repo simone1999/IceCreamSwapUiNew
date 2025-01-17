31b4483cb731b3aa2530252ed6ceb9190ebcd76e
SHAYA Mario <66465021+marioshaya@users.noreply.github.com>
2025-01-17 12:07:49 +0300

    update SEO configuration to reflect MermaidSwap branding and description

--- apps/web/next-seo.config.ts
+++ apps/web/next-seo.config.ts
@@ -1,19 +1,19 @@
 import { DefaultSeoProps } from 'next-seo'
 
 export const SEO: DefaultSeoProps = {
-  titleTemplate: '%s | IceCreamSwap',
-  defaultTitle: 'IceCreamSwap',
+  titleTemplate: '%s | MermaidSwap',
+  defaultTitle: 'MermaidSwap',
   description:
-    'Cheaper and faster than Uniswap? Discover PancakeSwap, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for CAKE.',
+    'MermaidSwap combines a decentralized exchange(DEX), fun games, and profit-sharing to revolutionize DeFi on KAIA.',
   twitter: {
     cardType: 'summary_large_image',
-    handle: '@PancakeSwap',
-    site: '@PancakeSwap',
+    handle: '@mermaidswap_xyz',
+    site: '@mermaidswap_xyz',
   },
   openGraph: {
-    title: '🥞 PancakeSwap - A next evolution DeFi exchange on BNB Smart Chain (BSC)',
+    title: 'MermaidSwap - Gaming and Rewards DeFi',
     description:
-      'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
-    images: [{ url: 'https://assets.pancakeswap.finance/web/og/hero.jpg' }],
+      'MermaidSwap combines a decentralized exchange(DEX), fun games, and profit-sharing to revolutionize DeFi on KAIA.',
+    images: [{ url: '/images/icecreamswap.png' }],
   },
 }
